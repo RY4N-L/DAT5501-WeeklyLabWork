12901cef3c334fecf1400a8102c6462fffe10682
RY4N-L <ryan.ly.lai@gmail.com>
2025-09-29 11:31:59 +0100

    Updated interest_rate

--- interest_rate.py
+++ interest_rate.py
@@ -1,14 +1,13 @@
-print("hello")
-def CompoundInterestCalculator(savings, annualInterestRate, years ):
-    
+def CompoundInterestCalculator(savings, annualInterestRate, years):
+
     multiply = annualInterestRate/100
     for i in range (years):
         savings *= 1 + multiply
         text1 = "Your savings after " + str(i+1) + " years is " + str(savings)
         print(text1)
-    number_of_years_to_double = 72/multiply
+    number_of_years_to_double = 72/annualInterestRate
 
-    text = "Your savings after " + str(years) + " years is " + str(savings) +". Your savings will double in " + str(number_of_years_to_double)
+    text = "Your savings after " + str(years) + " years is " + str(savings) +". Your savings will double in " + str(number_of_years_to_double) + " years"
     return text
 
-print(CompoundInterestCalculator(1000, 50, 10))
+print(CompoundInterestCalculator(10000, 3.5, 10))
