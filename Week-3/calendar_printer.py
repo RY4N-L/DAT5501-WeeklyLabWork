65c5872b411aba4271456322852fb2cbb60d6dc9
RY4N-L <ryan.ly.lai@gmail.com>
2025-10-06 12:34:30 +0100

    Initial Code for calendar printer

---
+++ Week-3/calendar_printer.py
@@ -0,0 +1,33 @@
+days_in_month = int(input("Please enter the number of days in the month:"))
+starting_day = str(input("Please enter the day of the week the month starts on:" ))
+
+#days_in_month = int(30)
+#starting_day = str("Friday")
+row_counter = int(0)
+first_row = True
+
+days_dict = {
+  "Monday": 1,
+  "Tuesday": 2,
+  "Wednesday": 3,
+  "Thursday": 4,
+  "Friday": 5,
+  "Saturday": 6,
+  "Sunday": 7
+}
+
+gaps_to_leave = int(days_dict[starting_day])
+row_counter += gaps_to_leave
+
+for i in range (days_in_month):
+    row_counter += 1
+    if first_row == True: # Runs if this is the first row - to print the dashed spaces
+        print ("S  M  T  W  T  F  S")
+        print ("-- " * gaps_to_leave, end="")
+        first_row = False
+    if row_counter==7: # If we are at the end of the week print and create a new line
+        row_counter = 0
+        print(str(i+1))
+    elif i<9: # If the date is <10th print an extra sace (for better alignment)
+        print(str(i+1) + "  ", end="")
+    else: print(str(i+1) + " ", end="")
