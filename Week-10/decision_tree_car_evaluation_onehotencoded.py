b63310c9382adfc381350337b43162906abd9199
RY4N-L <ryan.ly.lai@gmail.com>
2025-12-03 20:43:25 +0000

    Add: Feature importance analysis and onehotencoding

--- Week-10/decision_tree_car_evaluation_onehotencoded.py
+++ Week-10/decision_tree_car_evaluation_onehotencoded.py
@@ -8,6 +8,7 @@ from ucimlrepo import fetch_ucirepo
 from sklearn.model_selection import train_test_split, KFold, cross_val_score
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import OneHotEncoder
 
 # Fetch dataset 
 car_evaluation = fetch_ucirepo(id=19) 
@@ -16,21 +17,27 @@ car_evaluation = fetch_ucirepo(id=19)
 x = car_evaluation.data.features 
 y = car_evaluation.data.targets 
 
-# Change values to numerical
-x.replace({'vhigh':4, 'high':3, 'med':2, 'low':1, 
-           '5more': 5, 'more': 5, 
-           'small': 1, 'medium': 2, 'big': 3}, inplace=True)
+# Encode catagorical data with OneHotEncoder
+encoder = OneHotEncoder(sparse_output=False)
+x_encoded = encoder.fit_transform(x)
 
-# Show x an y data (uncomment to see)
-#print (x.head())
-#print(y.head())
+# Check encoded data is created correctly 
+#print (x_encoded)
+#print (encoder.get_feature_names_out())
+
+# Convert back to DataFrame with proper column names
+x_encoded = pd.DataFrame(x_encoded, columns=encoder.get_feature_names_out())
 
 # Format settings to show more rows and columns when printing dataframes
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', 100)
 
+# Show x an y data (uncomment to see)
+#print (x_encoded)
+#print(y.head())
+
 # Train/test split and decision tree classifier with a 70/30 split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0, stratify=y)
+x_train, x_test, y_train, y_test = train_test_split(x_encoded, y, test_size=0.3, random_state=0, stratify=y)
 
 # Create Decision Tree with a speified depth
 clf = DecisionTreeClassifier(max_depth= 5, random_state=0)
@@ -45,16 +52,15 @@ preds = clf.predict(x_test)
 kf = KFold(n_splits = 9, shuffle = True, random_state = 0)
 
 # Cross-validate
-cv_scores =  cross_val_score(clf, x, y, cv=kf)
+cv_scores =  cross_val_score(clf, x_encoded, y, cv=kf)
 print (f"Cross-Validation Scores:{cv_scores}")
 print (f"Highest Score:{np.max(cv_scores)}")
 print (f"Lowest Score:{np.mean(cv_scores)}")
 
-
 # Find feature importances
-# Create a DataFrame for readability
 
-feature_names = x.columns
+# Create a DataFrame for readability
+feature_names = x_encoded.columns
 importances = clf.feature_importances_
 
 feat_imp = pd.DataFrame({
@@ -70,22 +76,15 @@ feat_imp_nonzero = feat_imp[feat_imp["Importance"] > 0] # only include features 
 plt.figure(figsize=(8,5))
 plt.barh(feat_imp_nonzero["Feature"], feat_imp_nonzero["Importance"])
 
-
 plt.xlabel("Importance")
 plt.title("Feature Importances (Horizontal)")
 plt.tight_layout()
-#plt.show()
-
+plt.show()
 
 #print (f"Most important features:{clf.feature_importances_}")
 
 # Plot tree graphically
 plt.figure(figsize=(20,10))
-plot_tree(clf, feature_names=x.columns, class_names=y['class'].values, filled=True)
+plot_tree(clf, feature_names=x_encoded.columns, class_names=y['class'].values, filled=True)
 plt.tight_layout()
 plt.show()
-
-
-
-
-
--- Week-10/decision_tree_mushroom_onehotencoded.py
+++ Week-10/decision_tree_mushroom_onehotencoded.py
@@ -1,4 +1,4 @@
-## Decision Tree Classifier on Mushroom Dataset from  https://archive.ics.uci.edu/dataset/73/mushroom ##
+## Decision Tree Classifier on Car Evaluation Dataset from  https://archive.ics.uci.edu/dataset/73/mushroom ##
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,6 +7,7 @@ from ucimlrepo import fetch_ucirepo
 from sklearn.model_selection import train_test_split, KFold, cross_val_score
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import OneHotEncoder
 
 # Fetch dataset 
 mushroom = fetch_ucirepo(id=73) 
@@ -14,28 +15,33 @@ mushroom = fetch_ucirepo(id=73)
 # Data (as pandas dataframes) 
 x = mushroom.data.features 
 y = mushroom.data.targets 
-  
-# Change values to numerical
-x.replace({'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8, 'i':9, 'j':10,
-           'k':11, 'l':12, 'm':13, 'n':14, 'o':15, 'p':16, 'q':17, 'r':18, 's':19,
-           't':20, 'u':21, 'v':22, 'w':23, 'x':24, 'y':25, 'z':26,
-           '?':0}, inplace=True)
 
+# Encode catagorical data with OneHotEncoder
+encoder = OneHotEncoder(sparse_output=False)
+x_encoded = encoder.fit_transform(x)
 
-# Show x an y data (uncomment to see)
-#print (x.head())
-#print(y.head())
+# Check encoded data is created correctly 
+#print (x_encoded)
+#print (encoder.get_feature_names_out())
+
+# Convert back to DataFrame with proper column names
+x_encoded = pd.DataFrame(x_encoded, columns=encoder.get_feature_names_out())
 
 # Format settings to show more rows and columns when printing dataframes
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', 100)
 
+# Show x and y data (uncomment to see)
+#print (x_encoded)
+#print(y.head())
+
 # Train/test split and decision tree classifier with a 70/30 split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0, stratify=y)
+x_train, x_test, y_train, y_test = train_test_split(x_encoded, y, test_size=0.3, random_state=0, stratify=y)
 
-clf = DecisionTreeClassifier(random_state=0)
+# Create Decision Tree with a speified depth
+#clf = DecisionTreeClassifier(max_depth= 3, random_state=0)
+clf = DecisionTreeClassifier(random_state=0) # no depth specified
 clf.fit(x_train, y_train)
-
 
 # Evaluate on test set
 preds = clf.predict(x_test)
@@ -45,11 +51,11 @@ preds = clf.predict(x_test)
 kf = KFold(n_splits = 9, shuffle = True, random_state = 0)
 
 # Cross-validate
-cv_scores =  cross_val_score(clf, x, y, cv=kf)
+cv_scores =  cross_val_score(clf, x_encoded, y, cv=kf)
 print (cv_scores)
 
 # Find feature importances
-feature_names = x.columns
+feature_names = x_encoded.columns
 importances = clf.feature_importances_
 
 # Create a DataFrame for readability
@@ -71,14 +77,10 @@ plt.title("Feature Importances (Horizontal)")
 plt.tight_layout()
 plt.show()
 
+#print (f"Most important features:{clf.feature_importances_}")
 
 # Plot tree graphically
 plt.figure(figsize=(20,10))
-plot_tree(clf, feature_names=x.columns, class_names=y['poisonous'].values, filled=True)
+plot_tree(clf, feature_names=x_encoded.columns, class_names=y['poisonous'].values, filled=True)
 plt.tight_layout()
 plt.show()
-
-
-
-
-
